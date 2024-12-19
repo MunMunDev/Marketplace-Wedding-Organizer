f285558242bfd7ee9bd05e70372a07bfc39f6648
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2024-12-20 00:24:34 +0800

    Create register user and login user

---
+++ app/src/main/java/com/abcd/vian_marketplaceweddingorganizer/ui/activity/register/RegisterViewModel.kt
@@ -0,0 +1,36 @@
+package com.abcd.vian_marketplaceweddingorganizer.ui.activity.register
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abcd.vian_marketplaceweddingorganizer.data.database.api.ApiService
+import com.abcd.vian_marketplaceweddingorganizer.data.model.ResponseModel
+import com.abcd.vian_marketplaceweddingorganizer.utils.network.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val api: ApiService
+): ViewModel() {
+    private var _registerUser = MutableLiveData<UIState<ArrayList<ResponseModel>>>()
+
+    fun postRegisterUser(nama:String, alamat:String, nomorHp:String, username:String, password:String, sebagai:String){
+        viewModelScope.launch(Dispatchers.IO) {
+            _registerUser.postValue(UIState.Loading)
+            delay(1_000)
+            try {
+                val registerUser = api.addUser("", nama, alamat, nomorHp, username, password, sebagai)
+                _registerUser.postValue(UIState.Success(registerUser))
+            } catch (ex: Exception){
+                _registerUser.postValue(UIState.Failure("Gagal : ${ex.message}"))
+            }
+        }
+    }
+
+    fun getRegisterUser(): LiveData<UIState<ArrayList<ResponseModel>>> = _registerUser
+}
