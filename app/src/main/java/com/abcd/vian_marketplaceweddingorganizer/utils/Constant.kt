fd042eac2d3f62b4f1a7a8c2faadf4c60823e8b3
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-01-16 05:50:40 +0800

    Update constant

--- app/src/main/java/com/abcd/vian_marketplaceweddingorganizer/utils/Constant.kt
+++ app/src/main/java/com/abcd/vian_marketplaceweddingorganizer/utils/Constant.kt
@@ -1,19 +1,17 @@
 package com.abcd.vian_marketplaceweddingorganizer.utils
 
 object Constant {
-    const val BASE_URL = "http://192.168.17.2/"
-//    const val BASE_URL = "http://192.168.17.7/"
-//    const val BASE_URL = "http://192.168.1.154/"
-//    const val BASE_URL = "http://192.168.167.91/"
-//    const val BASE_URL = "https://e-portofolio.web.id/marketplace-wo/"
+    //    const val BASE_URL = "https://e-portofolio.web.id/"
+    const val BASE_URL = "http://192.168.17.5/"
     const val LOCATION_GAMBAR = "marketplace-wo/gambar/"
 
     const val STORAGE_PERMISSION_CODE = 10
 
-    const val MIDTRANS_SERVER_KEY = "\"SB-Mid-server-wA8kiwX70GwIoIOmlWkG5H-6\""
-    const val MIDTRANS_CLIENT_KEY = "SB-Mid-client-XKAPR8Xd9KBrS2m3"
-//    const val MIDTRANS_BASE_URL = "https://aplikasitugas17.000webhostapp.com/penjualan-plafon/api/response-midtrans.php/"
+    const val MIDTRANS_SERVER_KEY = "\"SB-Mid-server-3XXeHtewxccaCF8cPt3jTgY4\""
+    const val MIDTRANS_CLIENT_KEY = "SB-Mid-client-TVtOnG76Y3rS_nMh"
+    //    const val MIDTRANS_BASE_URL = "https://aplikasitugas17.000webhostapp.com/penjualan-plafon/api/response-midtrans.php/"
 //    const val MIDTRANS_BASE_URL = "https://aplikasi-skripsi-um-parepare.000webhostapp.com/penjualan-plafon/api/response-midtrans.php//"
-    const val MIDTRANS_BASE_URL = "https://aplikasi-tugas-kampus.my.id/penjualan-plafon/api/response-midtrans.php/"
+    const val MIDTRANS_BASE_URL = "https://e-portofolio.web.id/marketplace-wo/api/response-midtrans.php//"
+//    const val MIDTRANS_BASE_URL = "https://e-portofolio.web.id/marketplace-wo/Midtrans/examples/notification-handler.php/"
 
 }
